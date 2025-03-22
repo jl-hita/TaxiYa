86472e5cc8baea998fb207cf96c6221553c751d0
jl-hita <117595707+jl-hita@users.noreply.github.com>
2025-03-22 16:34:37 +0100

    Ruta.kt modificado

--- app/src/main/java/com/jlhipe/taxiya/model/Ruta.kt
+++ app/src/main/java/com/jlhipe/taxiya/model/Ruta.kt
@@ -1,13 +1,23 @@
 package com.jlhipe.taxiya.model
 
+import java.util.Date
+
 data class Ruta (
     val identificador: String = "", //Identificador único: se genera y se comprueba que no exista
-    val origen: String = "",    //Dirección con el formato de google maps API
-    val destino: String = "",   //Dirección con el formato de google maps API
     val conductor: String = "", //ID única del conductor, no mostrar en UI
     val cliente: String = "",   //ID única del cliente, no mostrar en UI
-    val precio: Int = 0,   //Precio en céntimos de euro
+    val origen: String = "",    //Dirección con el formato texto normal
+    val destino: String = "",   //Dirección con el formato texto normal
+    //val origenCoord: String = "",    //Dirección con el formato coordenadas
+    //val destinoCoord: String = "",   //Dirección con el formato coordenadas
+    //val dia: Date,     //Día que se inició la ruta
+    val dia: String = "",     //Día que se inició la ruta con formato aaaa/mm/dd
+    val precio: Int = 0,    //Precio en céntimos de euro
     val distancia: Float = 0.0F,    //Distancia en km con un decimal
+    val asignado:  Boolean = false,    //¿Está la ruta asignada a un taxista?
+    val haciaCliente: Boolean = false,    //¿Está el taxi yendo a recoger al usuario?
+    val haciaDestino: Boolean = false,    //¿Está el usuario en el taxi en ruta?
+    val finalizado: Boolean = false,    //¿Ha finalizado la ruta?
 ){
     /*
      * Para tener datos de prueba para la UI
@@ -15,8 +25,8 @@ data class Ruta (
     companion object {
         fun getData() : List<Ruta> {
             return listOf(
-                Ruta(generateRandomID(), "Carrer del Mestre Ramírez, 2, 46220 Picassent, Valencia, España", "Avinguda de Paiporta, 80, 46910 Benetússer, Valencia", "IDdeCoonductorInventada", "IDdeClienteInventada", 159, 11.8F),
-                Ruta(generateRandomID(), "Avinguda de Paiporta, 80, 46910 Benetússer, Valencia", "Carrer del Mestre Ramírez, 2, 46220 Picassent, Valencia, España", "IDdeCoonductorInventada", "IDdeClienteInventada", 159, 11.8F),
+                Ruta(generateRandomID(), "IDdeCoonductorInventada", "IDdeClienteInventada", "Carrer del Mestre Ramírez, 2, 46220 Picassent, Valencia, España", "Avinguda de Paiporta, 80, 46910 Benetússer, Valencia", "2025/03/18", 159, 11.8F, false, false, false, true),
+                Ruta(generateRandomID(), "IDdeCoonductorInventada", "IDdeClienteInventada", "Avinguda de Paiporta, 80, 46910 Benetússer, Valencia", "Carrer del Mestre Ramírez, 2, 46220 Picassent, Valencia, España", "2025/03/18", 159, 11.8F, false, false, false, true),
             )
         }
     }
--- app/src/main/java/com/jlhipe/taxiya/model/User.kt
+++ app/src/main/java/com/jlhipe/taxiya/model/User.kt
@@ -2,9 +2,12 @@ package com.jlhipe.taxiya.model
 
 data class User (
     val identificador: String = "",     //Identificador único: se genera y se comprueba que no exista
+    val primeraVez: Boolean = true,     //¿Es la primera vez que inicia la app?
     val esConductor: Boolean = false,   //¿Es conductor? Si false es usuario
+    val validado: Boolean = false,      //Un conductor debe estar validad para poder ejercer de taxista
     val nombre: String = "",
     val apellidos: String = "",
+    val enRuta: Boolean = false,        //¿Va en ruta en este momento?
+    val rutaActual: String = "",        //ID de la ruta actual si está en ruta (en cualquiera de sus estados)
 ){
-
 }
