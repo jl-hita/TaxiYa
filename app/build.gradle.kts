bdf34a27fc88112d550283f3e86c277f47e150e3
jl-hita <117595707+jl-hita@users.noreply.github.com>
2025-08-18 23:16:32 +0200

    Iconos de tienda y app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         applicationId = "com.jlhipe.taxiya"
         minSdk = 29
         targetSdk = 35
-        versionCode = 4
+        versionCode = 5
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
